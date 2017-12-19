7cc30cd9a24a966080e2898b2a02edce0b79d857
Matt Taylor <matthewbehantaylor@gmail.com>
2017-12-18 20:36:18 -0500

    start timer on keypress

--- script.js
+++ script.js
@@ -4,20 +4,39 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = 0
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
-
+function runTimer() {
+  theTimer.innerHTML = timer;
+  timer++;
+}
 
 // Match the text entered with the provided text on the page:
-
+function spellCheck() {
+  let textEntered = testArea.value;
+  console.log(textEntered);
+}
 
 // Start the timer:
-
+function start () {
+  let textEnteredLength = testArea.value.length;
+  if (textEnteredLength === 0) {
+    setInterval(runTimer, 10);
+  }
+  console.log(textEnteredLength);
+}
 
 // Reset everything:
+function reset () {
+  console.log("Reset!");
+}
 
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
